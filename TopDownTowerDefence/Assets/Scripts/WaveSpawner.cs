bb51c2370a15676c4420886a5e5c8aa68317cd7a
Jacob Patrick <fivestaroblivion@gmail.com>
2020-04-12 17:47:38 -0500

    Started working on monster spawner and global array

---
+++ TopDownTowerDefence/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+    #region Veriables
+    #region Public Veriables
+    public Transform enemyPrefab;
+        public float spawnInter = 5f;
+        #endregion
+    #region private veriables
+        private float waveCount = 1f;
+        private float countdown = 2f;
+        #endregion
+    #endregion
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0)
+        {
+            spawnWave();
+            countdown = spawnInter;
+        }
+    }
+
+    void spawnWave()
+    {
+        for (int i = 0; i < waveCount; i++)
+        {
+            
+        }
+
+        waveCount++;
+    }
+
+    void spwanEnemy(int enemyType, int enemyLevel, Transform spawnLocation)
+    {
+
+        Instantiate()
+    }
+
+    void getEnemy(int enemyType, int EnemyLevel)
+    {
+        
+    }
+}
